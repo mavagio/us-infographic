11fa887b5483b6d90e4df1eca6d25f2b60a6d364
martinavagyan <martinavagyan@gmail.com>
2018-06-27 14:58:17 +0200

    real time update of geochart data

--- client/src/app/components/google-chart/google-chart.component.ts
+++ client/src/app/components/google-chart/google-chart.component.ts
@@ -1,26 +1,38 @@
-import {Directive,ElementRef,Input,OnInit} from '@angular/core';
+import {Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 declare var google:any;
 declare var googleLoaded:any;
 @Directive({
   selector: '[GoogleChart]'
 })
-export class GoogleChartComponent implements OnInit {
+export class GoogleChartComponent implements OnInit, OnChanges {
+
   public _element:any;
   @Input('chartType') public chartType:string;
   @Input('chartOptions') public chartOptions: Object;
   @Input('chartData') public chartData: Object;
+
   constructor(public element: ElementRef) {
     this._element = this.element.nativeElement;
   }
+
   ngOnInit() {
+    this.render();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.render();
+  }
+
+  private render() {
     setTimeout(() =>{
         google.charts.load('current', {'packages':['corechart']});
         setTimeout(() =>{
           this.drawGraph(this.chartOptions,this.chartType,this.chartData,this._element)
-        },1000);
-      },1000
+        },1);
+      },1
     );
   }
+
   drawGraph (chartOptions,chartType,chartData,ele) {
     google.charts.setOnLoadCallback(drawChart);
     function drawChart() {
--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {GoogleChartComponent} from '../google-chart/google-chart.component';
 
 @Component({
@@ -6,23 +6,49 @@ import {GoogleChartComponent} from '../google-chart/google-chart.component';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges{
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("chang eis detecte");
+  }
+
+  ngOnInit(): void {
+  }
 
   constructor() { }
 
   public map_ChartData = [
-    ['Country', 'Popularity'],
-    ['Germany', 200],
-    ['United States', 300],
-    ['Brazil', 400],
+    ['State', 'Popularity'],
+    ['Georgia', 200],
+    ['Alabama', 300],
+    ['New Mexico', 400],
     ['Canada', 500],
-    ['France', 600],
-    ['RU', 700]
+    ['TX', 401],
+    ['California', 1000],
+    ['IA', 700]
   ];
 
-  public map_ChartOptions = {};
+  public map_ChartOptions = {
+    region: 'US',
+    displayMode: 'regions',
+    resolution: 'provinces',
+    legend: 'none',
+    enableRegionInteractivity: 'true',
+    sizeAxis: {minSize:1,  maxSize: 1000},
+    colorAxis: {minValue: 1, maxValue:1000,  colors: ['#B92B3D']}
+  };
 
-  ngOnInit() {
+  public addData() {
+    this.map_ChartData = [
+      ['State', 'Popularity'],
+      ['Alabama', 3000],
+      ['New Mexico', 400],
+      ['Canada', 500],
+      ['Texas', 600],
+      ['California', 3000],
+      ['IA', 700]
+    ];
+
+    this.map_ChartData.push(['Georgia', 200]);
+    console.log(this.map_ChartData);
   }
-
 }
